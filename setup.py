e40f6f724f1d4b8385d9e995993145bb3c32211c
anchorpoint <your-email@example.com>
2026-02-25 00:36:49 +0300

    chore: update license to GPLv3 and modernize CI/CD workflow

--- setup.py
+++ setup.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python3
+"""
+AEP Downgrader - Setup Script
+
+Copyright (C) 2024-2025  AEP Downgrader Contributors
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
@@ -16,16 +35,14 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.9",
     entry_points={
         'console_scripts': [
             'ae-downgrader=final_downgrader:main',
--- src/AEPdowngrader.py
+++ src/AEPdowngrader.py
@@ -2,7 +2,23 @@
 """
 AEP Downgrader - Modern GUI Application with Dark Theme
 Converts Adobe After Effects project files from newer versions to older ones
+
+Copyright (C) 2024-2025  AEP Downgrader Contributors
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
+
 import sys
 import os
 import shutil
